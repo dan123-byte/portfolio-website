d0451c2d302b18409a42070c90737990dcb54966
dan123-byte <danielandrei.tababa@gmail.com>
2025-09-03 15:28:38 +0800

    added the tech skills section and css design

--- javascript/main.js
+++ javascript/main.js
@@ -60,6 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// Project Decription - Selection
 document.addEventListener("DOMContentLoaded", () => {
   const detailsParagraph = document.querySelector("#project-details p");
 
@@ -75,5 +76,75 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener("click", () => {
       detailsParagraph.textContent = projectDescriptions[index];
     });
+  });
+});
+
+// Skills Menu - Shapes
+const items = document.querySelectorAll(".circle-menu h6");
+const panel = document.getElementById("infoPanel");
+
+const icons = {
+  "Development Tools": [
+    '<div class="icon-item" data-name="VS Code"><img src="assets/icons/vs.png" alt="VS Code"></div>',
+    '<div class="icon-item" data-name="Android Studio"><img src="assets/icons/as.png" alt="Android Studio"></div>',
+    '<div class="icon-item" data-name="pgAdmin 4"><img src="assets/icons/pgadmin.png" alt="pgAdmin 4"></div>'
+  ],
+  "Programming Languages": [
+    '<div class="icon-item" data-name="Python"><img src="assets/icons/py.png" alt="Python"></div>',
+    '<div class="icon-item" data-name="SQL"><img src="assets/icons/sql.png" alt="SQL"></div>',
+    '<div class="icon-item" data-name="JavaScript"><img src="assets/icons/js.png" alt="JavaScript"></div>',
+    '<div class="icon-item" data-name="HTML"><img src="assets/icons/html.png" alt="HTML"></div>',
+    '<div class="icon-item" data-name="CSS"><img src="assets/icons/css.png" alt="CSS"></div>',
+    '<div class="icon-item" data-name="XML"><img src="assets/icons/xml.png" alt="XML"></div>'
+  ],
+  "Frameworks": [
+    '<div class="icon-item" data-name="Flutter"><img src="assets/icons/flutter.png" alt="Flutter"></div>',
+    '<div class="icon-item" data-name="FastAPI"><img src="assets/icons/fa.png" alt="FastAPI"></div>',
+    '<div class="icon-item" data-name="Odoo"><img src="assets/icons/odoo.png" alt="Odoo"></div>',
+    '<div class="icon-item" data-name="React"><img src="assets/icons/react.png" alt="React"></div>'
+  ],
+  "Databases": [
+    '<div class="icon-item" data-name="PostgreSQL"><img src="assets/icons/postgresql.png" alt="PostgreSQL"></div>'
+  ],
+  "Version Control": [
+    '<div class="icon-item" data-name="Git"><img src="assets/icons/git.png" alt="Git"></div>',
+    '<div class="icon-item" data-name="GitHub"><img src="assets/icons/github.png" alt="GitHub"></div>'
+  ],
+  "Testing Tools": [
+    '<div class="icon-item" data-name="sqlmap"><img src="assets/icons/sqlmap.png" alt="sqlmap"></div>',
+    '<div class="icon-item" data-name="Apache Jmeter"><img src="assets/icons/apache.png" alt="Apache Jmeter"></div>',
+    '<div class="icon-item" data-name="Open SSL"><img src="assets/icons/open.png" alt="Open SSL"></div>',
+    '<div class="icon-item" data-name="Oracle VirtualBox"><img src="assets/icons/ovb.png" alt="Oracle VirtualBox"></div>',
+    '<div class="icon-item" data-name="UptimeRobot"><img src="assets/icons/uptimerobot.png" alt="UptimeRobot"></div>'
+  ]
+};
+
+items.forEach(item => {
+  item.addEventListener("click", () => {
+    const side = item.dataset.side;
+    panel.style.display = "block";
+
+    const iconSet = (icons[item.textContent] || []).join("");
+
+    panel.classList.remove("left", "right");
+    panel.classList.add(side);
+
+    panel.innerHTML = `
+      <div class="icons-container">
+        <div class="icon-grid">
+          ${iconSet}
+        </div>
+      </div>
+    `;
+  });
+});
+
+// Highlight Selected
+const menuItems = document.querySelectorAll('.circle-menu h6');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    menuItems.forEach(i => i.classList.remove('active'));
+    item.classList.add('active');
   });
 });
